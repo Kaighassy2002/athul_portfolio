aee13660fccb133b50be3df71658358da953fa91
Kaighassy2002 <kaighassysuresh@gmail.com>
2025-03-13 21:16:07 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 style={{display:'flex', alignItems:'center',justifyContent:'center'}}>Site Maintenance.</h1>
+    </>
+  )
+}
+
+export default App
